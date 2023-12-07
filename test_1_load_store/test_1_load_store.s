eb8a91f2632e93415ca04aec9022cd93dc1e45d7
CompuGuy99 <alex@compuguy.dev>
2023-12-07 14:49:26 -0700

    changed test1 to use all the store and load instructions

--- test_1_load_store/test_1_load_store.s
+++ test_1_load_store/test_1_load_store.s
@@ -1,17 +1,29 @@
-# Test the 8 instructions that interact with memory
+# RISC-V Assembly Program to Check Load and Store Variants
+
 .data
-    array: .word  10, 20, 30, 40, 50
+    array:  .space  16      # Allocate 16 bytes of space for the array
 
 .text
-    la x5, array
+    la x1, array            # Load the base address of the array into x1
 
-    lw x1, 0(x5)
-    lw x2, 4(x5)
+    # Store values to memory using different store instructions
+    addi x2, x0, 42         # Load immediate value 42 to t1
+    sb x2, 0(x1)            # Store byte (8 bits) to array[0]
+    sh x2, 2(x1)            # Store halfword (16 bits) to array[2]
+    sw x2, 4(x1)            # Store word (32 bits) to array[4]
 
-    add x3, x1, x2
-    sw x3, 8(x5)
+    # Load values from memory using different load instructions
+    lb x2, 0(x1)            # Load byte from array[0] to t2
+    lh x3, 2(x1)            # Load halfword from array[2] to t3
+    lw x4, 4(x1)            # Load word from array[4] to t4
+    lhu x5, 8(x1)           # Load upper halfword from array[8] to t5
+    lbu x6, 8(x1)           # Load upper byte from array[8] into x6
 
-    lw x4, 8(x5)
+    # Check results
+    # You can customize this part based on your specific checks or requirements
+    # For simplicity, this example just uses registers to hold the values.
+    # You may want to print or compare the values as needed.
 
-    # infinite loop
-
+    # Exit the program
+    addi x7, x0, 10         # System call code for program exit
+    ecall                   # Make the system call
