4a2d735a245e43c4c41455b92d868f2668ad9fb3
CompuGuy99 <alex@compuguy.dev>
2023-12-07 22:22:18 -0700

    Renamed all registers in test 4 to real registers

--- test_4_opimm/test_4_opimm.s
+++ test_4_opimm/test_4_opimm.s
@@ -3,76 +3,76 @@
 # Data section
 .data
   array:  .space 16
-  msg1:   .asciiz "Test passed!\n"
-  msg2:   .asciiz "Test failed!\n"
+  msg1:   .word 0
+  msg2:   .word 1
 
 # Text section
 .text
   main:
     # Arithmetic instructions with immediate variants
-    li a0, 2
-    li a1, 3
-    addi a2, a0, 5       # a2 = a0 + 5
+    li x1, 2
+    li x2, 3
+    addi x3, x1, 5       # x3 = x1 + 5
 
     # Logical instructions with immediate variants
-    li a4, 1
-    li a5, 0
-    andi a6, a4, 3       # a6 = a4 & 3
-    ori  a7, a5, 5       # a7 = a5 | 5
-    xori a8, a6, 7       # a8 = a6 ^ 7
+    li x5, 1
+    li x6, 0
+    andi x7, x5, 3       # x7 = x5 & 3
+    ori  x8, x6, 5       # x8 = x6 | 5
+    xori x9, x7, 7       # x9 = x7 ^ 7
 
     # SLT and SLTU instructions with immediate variants
-    li a9, 5
-    li a10, 3
-    slti a11, a9, 8      # a11 = (a9 < 8)
-    sltiu a12, a10, 2    # a12 = (a10 < 2) unsigned
+    li x10, 5
+    li x11, 3
+    slti x12, x10, 8      # x12 = (x10 < 8)
+    sltiu x13, x11, 2    # x13 = (x11 < 2) unsigned
 
     # Load and store instructions with immediate variants
-    li a13, 42
-    sw a13, array        # Store a13 in array
-    lw a14, array        # Load from array to a14
+    li x14, 42
+    sw x14, array        # Store x14 in array
+    lw x15, array        # Load from array to x15
 
     # Shift instructions with immediate variants
-    li a15, 8
-    slli a16, a15, 2     # a16 = a15 << 2 (shift left logical immediate)
-    srli a17, a16, 1     # a17 = a16 >> 1 (shift right logical immediate)
-    srai a18, a16, 1     # a18 = a16 >> 1 (shift right arithmetic immediate)
+    li x5, 8
+    slli x6, x5, 2     # x6 = x5 << 2 (shift left logical immediate)
+    srli x7, x6, 1     # x7 = x6 >> 1 (shift right logical immediate)
+    srai x8, x6, 1     # x8 = x6 >> 1 (shift right arithmetic immediate)
 
     # Branch instructions
-    bne a13, a14, fail   # Branch if not equal
-    bnez a11, fail       # Branch if a9 < 8 is true
-    bnez a18, fail       # Branch if a16 >> 1 is not zero
+    bne x14, x15, fail   # Branch if not equal
+    bnez x12, fail       # Branch if x10 < 8 is true
+    bnez x8, fail       # Branch if x6 >> 1 is not zero
     j success            # Jump to success label
 
 fail:
     j fail_exit
 
 success:
-    li a0, 0
+    li x1, 0
     j success_exit
 
 fail_exit:
     # Print failure message
-    li a0, 1
-    li a1, msg2
-    li a2, 13   # Length of the string
-    li a7, 64   # Write syscall number
+    li x1, 1
+    li x2, msg2
+    li x3, 13   # Length of the string
+    li x8, 64   # Write syscall number
     ecall
 
     # Exit program
-    li a7, 93   # Exit syscall number
-    li a0, 1
+    li x8, 93   # Exit syscall number
+    li x1, 1
     ecall
 
 success_exit:
     # Print success message
-    li a0, 1
-    li a1, msg1
-    li a2, 14   # Length of the string
-    li a7, 64   # Write syscall number
+    li x1, 1
+    li x2, msg1
+    li x3, 14   # Length of the string
+    li x8, 64   # Write syscall number
     ecall
 
     # Exit program
-    li a7, 93   # Exit syscall number
-    li a0, 0
+    li x8, 93   # Exit syscall number
+    li x1, 0
     ecall
