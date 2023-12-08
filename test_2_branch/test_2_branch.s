d82565dd0a26bed54fdc1a5db459ea25ea6a3c16
CompuGuy99 <alex@compuguy.dev>
2023-12-07 22:10:08 -0700

    Fix stuff for branch test

--- test_2_branch/test_2_branch.s
+++ test_2_branch/test_2_branch.s
@@ -1,140 +1,140 @@
 # RISC-V 32E Assembly Program to Explicitly Test Branching Instructions
 
 .data
-msg_success:    .asciiz "Branching instructions test passed!\n"
-msg_failure:    .asciiz "Branching instructions test failed!\n"
+msg_success:    .word 0
+msg_failure:    .word 1
 
 .text
 .global _start
 
 _start:
     # Initialize registers
-    li  a0, 1      # Initialize a counter
-    li  a1, 10     # Set the loop limit
+    li  x1, 1      # Initialize a counter
+    li  x2, 10     # Set the loop limit
 
     # BEQ Test
-    beq a0, a1, beq_success
+    beq x1, x2, beq_success
     j   beq_failure
 
 beq_success:
     # Print success message for BEQ
-    la  a0, msg_success
-    li  a7, 4      # Print string system call
+    la  x1, msg_success
+    li  x15, 4      # Print string system call
     ecall
     j   next_test
 
 beq_failure:
     # Print failure message for BEQ
-    la  a0, msg_failure
-    li  a7, 4      # Print string system call
+    la  x1, msg_failure
+    li  x15, 4      # Print string system call
     ecall
     j   end_program
 
 next_test:
     # Increment counter and check if all tests are done
-    addi a0, a0, 1
-    bne a0, 7, test_loop  # Change 7 to the total number of tests
+    addi x1, x1, 1
+    bne x1, 7, test_loop  # Change 7 to the total number of tests
 
     # If all tests are done, exit the program
-    li  a7, 10     # Exit system call
+    li  x15, 10     # Exit system call
     ecall
 
 test_loop:
     # BNE Test
-    bne a0, a1, bne_success
+    bne x1, x2, bne_success
     j   bne_failure
 
 bne_success:
     # Print success message for BNE
-    la  a0, msg_success
-    li  a7, 4      # Print string system call
+    la  x1, msg_success
+    li  x15, 4      # Print string system call
     ecall
     j   next_test
 
 bne_failure:
     # Print failure message for BNE
-    la  a0, msg_failure
-    li  a7, 4      # Print string system call
+    la  x1, msg_failure
+    li  x15, 4      # Print string system call
     ecall
     j   end_program
 
     # BLT (signed) Test
-    li  a2, -5     # Set a negative number
-    blt a2, a1, blt_signed_success
+    li  x3, -5     # Set a negative number
+    blt x3, x2, blt_signed_success
     j   blt_signed_failure
 
 blt_signed_success:
     # Print success message for BLT (signed)
-    la  a0, msg_success
-    li  a7, 4      # Print string system call
+    la  x1, msg_success
+    li  x15, 4      # Print string system call
     ecall
     j   next_test
 
 blt_signed_failure:
     # Print failure message for BLT (signed)
-    la  a0, msg_failure
-    li  a7, 4      # Print string system call
+    la  x1, msg_failure
+    li  x15, 4      # Print string system call
     ecall
     j   end_program
 
     # BLTU Test
-    li  a3, 0xFFFFFFFF # Set a large positive unsigned number
-    bltu a3, a1, bltu_success
+    li  x4, 0xFFFFFFFF # Set a large positive unsigned number
+    bltu x4, x2, bltu_success
     j   bltu_failure
 
 bltu_success:
     # Print success message for BLTU
-    la  a0, msg_success
-    li  a7, 4      # Print string system call
+    la  x1, msg_success
+    li  x15, 4      # Print string system call
     ecall
     j   next_test
 
 bltu_failure:
     # Print failure message for BLTU
-    la  a0, msg_failure
-    li  a7, 4      # Print string system call
+    la  x1, msg_failure
+    li  x15, 4      # Print string system call
     ecall
     j   end_program
 
     # BGE (signed) Test
-    li  a4, 15     # Set a positive number
-    bge a4, a1, bge_signed_success
+    li  x5, 15     # Set a positive number
+    bge x5, x2, bge_signed_success
     j   bge_signed_failure
 
 bge_signed_success:
     # Print success message for BGE (signed)
-    la  a0, msg_success
-    li  a7, 4      # Print string system call
+    la  x1, msg_success
+    li  x15, 4      # Print string system call
     ecall
     j   next_test
 
 bge_signed_failure:
     # Print failure message for BGE (signed)
-    la  a0, msg_failure
-    li  a7, 4      # Print string system call
+    la  x1, msg_failure
+    li  x15, 4      # Print string system call
     ecall
     j   end_program
 
     # BGEU Test
-    li  a5, 20     # Set a positive unsigned number
-    bgeu a5, a1, bgeu_success
+    li  x6, 20     # Set a positive unsigned number
+    bgeu x6, x2, bgeu_success
     j   bgeu_failure
 
 bgeu_success:
     # Print success message for BGEU
-    la  a0, msg_success
-    li  a7, 4      # Print string system call
+    la  x1, msg_success
+    li  x15, 4      # Print string system call
     ecall
     j   next_test
 
 bgeu_failure:
     # Print failure message for BGEU
-    la  a0, msg_failure
-    li  a7, 4      # Print string system call
+    la  x1, msg_failure
+    li  x15, 4      # Print string system call
     ecall
     j   end_program
 
 end_program:
     # Exit program
-    li  a7, 10     # Exit system call
+    li  x15, 10     # Exit system call
     ecall
